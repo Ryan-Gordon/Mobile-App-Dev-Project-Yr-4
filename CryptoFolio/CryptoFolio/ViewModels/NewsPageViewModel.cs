ab6cf3b78e6b37f36cf0954672493c03aafc721e
Ryan Gordon <ryangordon210@gmail.com>
2017-11-22 19:48:18 +0000

    Added View Model for news page

---
+++ CryptoFolio/CryptoFolio/ViewModels/NewsPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CryptoFolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Core.Data;
+
+namespace CryptoFolio.ViewModels
+{
+    public class NewsPageViewModel : ShellViewModel
+    {
+        
+    }
+}
+
