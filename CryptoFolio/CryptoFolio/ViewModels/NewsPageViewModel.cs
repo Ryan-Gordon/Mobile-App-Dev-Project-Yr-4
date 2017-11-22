5e591b91dfc6b18994cd1bea9132bdc1f83a61c3
Ryan Gordon <ryangordon210@gmail.com>
2017-11-22 19:49:27 +0000

    Example data for news page

--- CryptoFolio/CryptoFolio/ViewModels/NewsPageViewModel.cs
+++ CryptoFolio/CryptoFolio/ViewModels/NewsPageViewModel.cs
@@ -10,7 +10,27 @@ namespace CryptoFolio.ViewModels
 {
     public class NewsPageViewModel : ShellViewModel
     {
-        
+        private int currentCount = 0;
+        public IncrementalLoadingCollection<Article> Months { get; set; }
+        public NewsPageViewModel()
+        {
+            Months = new IncrementalLoadingCollection<Article>(this.GetMoreItems) { BatchSize = 5 };
+        }
+         
+
+        private async Task<IEnumerable<Article>> GetMoreItems(uint count)
+        {
+            await Task.Delay(1000);
+            if (this.currentCount >= 100)
+            {
+                return null;
+            }
+
+            var random = new Random(Environment.TickCount);
+            var result = Enumerable.Range(this.currentCount, (int)count).Select(x => new Article { Label = "item " + x.ToString(), Height = random.Next(50, 150) }).ToList();
+            currentCount += (int)count;
+            return result;
+        }
     }
 }
 
