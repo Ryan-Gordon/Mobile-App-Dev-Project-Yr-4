e35ef029ebb2c796aa8cc97aa03a7994e86756d1
Ryan Gordon <ryangordon210@gmail.com>
2017-11-22 19:49:07 +0000

    News Page
    This should hold some crypto news data.

---
+++ CryptoFolio/CryptoFolio/Views/NewsPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml.Controls;
+using CryptoFolio.ViewModels;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CryptoFolio.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    ///
+
+    
+    public sealed partial class NewsPage : Page
+    {
+        private NewsPageViewModel ViewModel
+        {
+            get { return DataContext as NewsPageViewModel; }
+        }
+        public NewsPage()
+        {
+            InitializeComponent();
+            NewsPageViewModel vm = new NewsPageViewModel();
+        }
+    }
+}
