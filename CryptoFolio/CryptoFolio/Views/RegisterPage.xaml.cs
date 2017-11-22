7d02ece2a35689f108b90a231327c6b4bea59ee2
Ryan Gordon <ryangordon210@gmail.com>
2017-11-22 13:02:02 +0000

    Created Register View

---
+++ CryptoFolio/CryptoFolio/Views/RegisterPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using CryptoFolio.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace CryptoFolio.Views
+{
+    public sealed partial class RegisterPage : Page
+    {
+        private RegisterViewModel ViewModel
+        {
+            get { return DataContext as RegisterViewModel; }
+        }
+
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
