93c96789102e5fa19fd1c6ccd225769b06e51e96
Ryan Gordon <ryangordon210@gmail.com>
2017-11-22 14:34:04 +0000

    Delagating user creation to model
    Have implementated a function which instantiates an instance of the viewmodel and calls its create user method passing the entered params

--- CryptoFolio/CryptoFolio/Views/RegisterPage.xaml.cs
+++ CryptoFolio/CryptoFolio/Views/RegisterPage.xaml.cs
@@ -3,6 +3,7 @@
 using CryptoFolio.ViewModels;
 
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
 
 namespace CryptoFolio.Views
 {
@@ -17,5 +18,34 @@ namespace CryptoFolio.Views
         {
             InitializeComponent();
         }
+        //takes the details provided in the view and attempts to create a user in the viewmodel
+        private async void registerBT_Click(object sender, RoutedEventArgs e)
+        {
+            if (registerUsernameTextBox.Text != "" && registerPasswordTextBox.Password != "")
+            {
+                // create a new user
+                RegisterViewModel vm = new RegisterViewModel();
+                if (await vm.createUser(registerUsernameTextBox.Text, registerPasswordTextBox.Password))
+                {
+                    //If the result of create user is true; We successfully register the details
+                    Frame.Navigate(typeof(MainPage));
+                }
+                else
+                {
+                    errorTextBlock.Text = "Error occured on server on user exists. Try login";
+                }
+            }
+            else
+            {
+                //tell user to include all details needed
+                errorTextBlock.Text = "Error! A Username AND Password must be entered!";
+            } 
+        }
+        //Navigate to the login page 
+        private void loginBT_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(LoginPage));
+        } 
     }
 }
+
