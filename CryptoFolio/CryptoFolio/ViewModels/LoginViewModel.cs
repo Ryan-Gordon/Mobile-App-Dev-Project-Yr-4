b2fa01add8de3c837e1ab56492657f8dbf9b6731
Ryan Gordon <ryangordon210@gmail.com>
2017-12-05 17:15:18 +0000

    Added files for Login view and View model

---
+++ CryptoFolio/CryptoFolio/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using GalaSoft.MvvmLight;
+
+namespace CryptoFolio.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+
+        public LoginViewModel()
+        {
+
+        }
+    }
+}
---
+++ CryptoFolio/CryptoFolio/Views/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using CryptoFolio.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace CryptoFolio.Views
+{
+    public sealed partial class LoginPage : Page
+    {
+        private LoginViewModel ViewModel
+        {
+            get { return DataContext as LoginViewModel; }
+        }
+
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
