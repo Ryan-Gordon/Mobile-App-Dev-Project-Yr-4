8c11605622cd94aacbda26d32e77651159577b43
Ryan Gordon <ryangordon210@gmail.com>
2017-12-07 18:38:06 +0000

    Add coin form

---
+++ CryptoFolio/CryptoFolio/ViewModels/AddCoinViewModel.cs
@@ -0,0 +1,45 @@
+﻿
+using CryptoFolio.Services;
+using GalaSoft.MvvmLight;
+using NoobsMuc.Coinmarketcap.Client;
+using System.Collections;
+using System.Diagnostics;
+using static CryptoFolio.Views.AddCoinPage;
+
+namespace CryptoFolio.ViewModels
+{
+    public class AddCoinViewModel : ViewModelBase
+    {
+        public AddCoinViewModel()
+        {
+            test();
+            get_All_Names();
+
+        }
+
+        public void test()
+        {
+            Currency test = CoinMarketCapService.getTicker();
+            Debug.WriteLine(CoinMarketCapService.getTicker());
+            new Item() { Amount = 0, Coin = Coin.Bitcoin, watchOnly = true, Notes = "First Transaction" };
+
+            Debug.WriteLine(test.Id);
+            Debug.WriteLine(test.PriceUsd);
+        }
+
+        public ArrayList get_All_Names()
+        {
+            //Currency test = CoinMarketCapService.getAll();
+            Debug.WriteLine(CoinMarketCapService.getAll());
+            ArrayList currencyList= new ArrayList();
+
+            foreach (Currency coin in CoinMarketCapService.getAll())
+            {
+                Debug.WriteLine(coin.Name);
+                currencyList.Add(coin.Name);
+            }
+
+            return currencyList;
+        }
+    }
+}
---
+++ CryptoFolio/CryptoFolio/Views/AddCoinPage.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+using CryptoFolio.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+using Telerik.Data.Core;
+
+using Windows.UI.Xaml;
+
+using Windows.UI.Xaml.Controls;
+
+namespace CryptoFolio.Views
+{
+    public sealed partial class AddCoinPage : Page
+    {
+        private AddCoinViewModel ViewModel
+        {
+            get { return DataContext as AddCoinViewModel; }
+        }
+
+        public AddCoinPage()
+        {
+            InitializeComponent();
+
+
+            this.DataContext = new Item() { Amount = 0, Coin = Coin.Bitcoin, watchOnly = true, Notes = "First Transaction" };
+
+        }
+
+       
+
+
+
+        public class Item
+
+        {
+            [Display(Group = "Add coin to portfolio", Header = "Coin")]
+
+            public Coin Coin { get; set; }
+
+            
+
+            [Display(Header = "Amount")]
+
+            public double Amount { get; set; }
+
+
+
+            
+
+            [Display(Group = "Watch Only ? Holdings wont be tracked", Header = "Watch only?")]
+
+            public bool watchOnly { get; set; }
+
+            [Display(Header = "Notes")]
+
+            public string Notes { get; set; }
+
+        }
+
+        public enum Coin
+
+        {
+
+            Bitcoin,
+Ethereum,
+BCash,
+IOTA,
+Ripple,
+Dash,
+Litecoin,
+BGold,
+Monero,
+Cardano,
+EthereumClassic,
+StellarLumens,
+NEM,
+NEO,
+EOS,
+BitConnect,
+Lisk,
+Stratis,
+Qtum,
+MonaCoin,
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+
+
+    public class DataFormGroupHeaderSelector : DataTemplateSelector
+
+    {
+
+        public DataTemplate HeaderTemplate { get; set; }
+
+        protected override Windows.UI.Xaml.DataTemplate SelectTemplateCore(object item, Windows.UI.Xaml.DependencyObject container)
+
+        {
+
+            if (item.ToString() == "Additional Information")
+
+            {
+
+                return this.HeaderTemplate;
+
+            }
+
+
+
+            return base.SelectTemplateCore(item, container);
+
+        }
+
+    }
+    
+
+}
