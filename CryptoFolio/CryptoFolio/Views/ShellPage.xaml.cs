304c1095060645b7cc84db79cc3779bdb472bb7f
Ryan Gordon <ryangordon210@gmail.com>
2017-12-07 22:38:44 +0000

    Shell page for UI

---
+++ CryptoFolio/CryptoFolio/Views/ShellPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using CryptoFolio.Services;
+using CryptoFolio.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace CryptoFolio.Views
+{
+    public sealed partial class ShellPage : Page
+    {
+        private ShellViewModel ViewModel
+        {
+            get { return DataContext as ShellViewModel; }
+        }
+
+        public ShellPage()
+        {
+            InitializeComponent();
+            DataContext = ViewModel;
+            ViewModel.Initialize(shellFrame);
+        }
+    }
+}
